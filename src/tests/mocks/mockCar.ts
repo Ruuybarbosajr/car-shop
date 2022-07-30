6e5f899828bd1239f69262b42b0b29c483d2b74f
Ruy Barbosa Jr <ruybarbosajr@pop-os.localdomain>
2022-07-30 17:11:33 -0300

    feat: create tests

---
+++ src/tests/mocks/mockCar.ts
@@ -0,0 +1,21 @@
+import { ICar } from '../../interfaces/ICar';
+
+export const createdCar: ICar = {
+	"doorsQty": 4,
+	"seatsQty": 5,
+	"model": "Marea",
+	"year": 2003,
+	"color": "Prata",
+	"buyValue": 30,
+}
+
+export const createdCarWithId: ICar & { _id: string, __v: number } = {
+	"doorsQty": 4,
+	"seatsQty": 5,
+	"model": "Marea",
+	"year": 2003,
+	"color": "Prata",
+	"buyValue": 30,
+	"_id": "62e5657e8051d02a82a20880",
+	"__v": 0
+}
---
+++ src/tests/unit/models/Car.test.ts
@@ -0,0 +1,29 @@
+// template para criação dos testes de cobertura da camada de model
+import * as sinon from 'sinon';
+import chai from 'chai';
+import { Model } from 'mongoose';
+import { createdCar, createdCarWithId } from '../../mocks/mockCar'
+import CarModel from '../../../models/Car.model';
+const { expect } = chai;
+
+describe('Testa camada model de Car', () => {
+
+  const carModel = new CarModel();
+
+  before(async () => {
+    sinon.stub(Model, 'create')
+    .onCall(0)
+    .resolves(createdCarWithId);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  describe('Em caso de sucesso', () => {
+    it('Deve criar e retornar um objeto', async () => {
+      const newCar = await carModel.create(createdCar)
+      expect(newCar).to.be.eql(createdCarWithId);
+    });
+  });
+});
